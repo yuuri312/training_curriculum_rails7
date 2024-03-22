268c07002c699724e12a39d91601d08fad7ae5cc
yuuri312 <yz5@outlook.jp>
2024-03-22 16:33:24 +0900

    再

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,45 +2,46 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_week
     @plan = Plan.new
   end
 
   # 予定の保存
   def create
-    @Plan = Plan.new(plan_params)
-    
-  # 予定の保存
-  def create
-    @plan = Plan.new(plan_params)
-    
-    if @plan.save
-      redirect_to root_path, notice: '予定を保存しました。'
-    else
-      redirect_to root_path, alert: '予定の保存に失敗しました。'
-    end
-  end  
-end
-  
+    # binding.pry
+    Plan.create(plan_params)
+    redirect_to action: :index
+  end
+
   private
 
   def plan_params
     params.require(:plan).permit(:date, :plan)
   end
 
-  def getWeek
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
-    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
-    @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
-    @week_days= []
    
-    (0..6).each do |x|
-      today_plans = Plan.where(date: @todays_date+x)
-      days={month: (@todays_date + x).month,date: (@todays_date+x).strftime("%-d"),wday: wdays[(@todays_date+x).wday],plans: today_plans}
+    @week_days = []
+    @todays_date = Date.today
+
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
+
+    7.times do |x|
+      today_plans = []
+      plans.each do |plan|
+        today_plans.push(plan.plan) if plan.date == @todays_date + x
+      end
+
+      wday_num = Date.today.wday + x
+
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+
+      days = {month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
-     end 
-end 
-   end
+
+      end
+    end
+  end
